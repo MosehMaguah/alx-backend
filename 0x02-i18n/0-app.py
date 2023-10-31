c2357d5213cb36e53283da59a7f4a476a725ae63
mosehmaguah <mosehmaguah@gmail.com>
2023-10-31 13:30:07 +0000

    home route

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" A Basic Flask app """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """ A basic Flask app """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
