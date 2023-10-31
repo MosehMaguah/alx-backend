cf0f2039303b939a5ff77f32affcfebee6b3639f
mosehmaguah <mosehmaguah@gmail.com>
2023-10-31 14:20:34 +0000

    flask-babel app

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Basic Flask app and Babel setup, Get locale from request """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+""" instantiate the Babel object """
+
+
+class Config(object):
+    """ A config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+""" Use config  class as config for Flask app """
+
+
+@app.route('/')
+def home():
+    """ A basic Flask app """
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """ determining the best match with our supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
